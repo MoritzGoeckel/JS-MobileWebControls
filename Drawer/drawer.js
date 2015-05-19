ccc0b29314072e0c2b350e87e6b7ba158e7c090b
MoritzGoeckel <moritz.goeckel@gmail.com>
2015-05-19 15:12:32 +0200

    styled the basic drawer

--- Drawer/drawer.js
+++ Drawer/drawer.js
@@ -1,14 +1,38 @@
-$(function () { //Init
-  console.log("Init");
+function Drawer()
+{
+  $(function () { //Init
+    console.log("Init");
 
-  $("#open-drawer-btn").click(function(event){
+    $("#open-drawer-btn").click(function(event){
 
-    var duration = {duration: 200, specialEasing: "linear"};
+      var duration = {duration: 200, specialEasing: "linear"};
 
-    if($("#drawer").css("left") != "0px")
-      $("#drawer").animate({"left": "0px"}, duration);
-    else
-      $("#drawer").animate({"left": "-300px"}, duration);
+      if($("#drawer").css("left") != "0px")
+        $("#drawer").animate({"left": "0px"}, duration);
+      else
+        $("#drawer").animate({"left": "-300px"}, duration);
+    });
+
   });
 
-});
+  this.close = function(){
+    $("#drawer").animate({"left": "-300px"}, duration);
+  };
+
+  this.open = function(){
+    $("#drawer").animate({"left": "0px"}, duration);
+  };
+
+  this.toggle = function(){
+    if(isOpen())
+      close();
+    else
+      open();
+  };
+
+  this.isOpen = function(){
+    return $("#drawer").css("left") == "0px";
+  };
+}
+
+var drawer = new Drawer();

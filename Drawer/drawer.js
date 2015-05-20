9ab6766bcd4be37e526dfcb95b001049de59eea3
MoritzGoeckel <moritz.goeckel@gmail.com>
2015-05-20 17:56:31 +0200

    Made it more oop

--- Drawer/drawer.js
+++ Drawer/drawer.js
@@ -1,38 +1,34 @@
+$(function () { //Init
+  console.log("Init");
+  drawer = new Drawer();
+
+  $("#open-drawer-btn").click(function(event){
+    drawer.toggle();
+  });
+});
+
 function Drawer()
 {
-  $(function () { //Init
-    console.log("Init");
-
-    $("#open-drawer-btn").click(function(event){
-
-      var duration = {duration: 200, specialEasing: "linear"};
-
-      if($("#drawer").css("left") != "0px")
-        $("#drawer").animate({"left": "0px"}, duration);
-      else
-        $("#drawer").animate({"left": "-300px"}, duration);
-    });
-
-  });
+  var duration = {duration: 200, specialEasing: "linear"};
 
   this.close = function(){
     $("#drawer").animate({"left": "-300px"}, duration);
+    $("#content-opac").css("width", "0%");
   };
 
   this.open = function(){
     $("#drawer").animate({"left": "0px"}, duration);
+    $("#content-opac").css("width", "100%");
   };
 
   this.toggle = function(){
-    if(isOpen())
-      close();
+    if(this.isOpen())
+      this.close();
     else
-      open();
+      this.open();
   };
 
   this.isOpen = function(){
     return $("#drawer").css("left") == "0px";
   };
 }
-
-var drawer = new Drawer();

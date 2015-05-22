b43f606666b63176fa28e22b30b40f8f4ef2cf5c
MoritzGoeckel <moritz.goeckel@gmail.com>
2015-05-22 17:32:57 +0200

    Fixed a bug

--- Chart/chart.js
+++ Chart/chart.js
@@ -2,7 +2,7 @@ $(function () { //Init
   console.log("Init");
   chart = new Chart();
 
-  chart.setData("[{x: 200, y: 10}, {x: 200, y: 10}, {x: 240, y: 15}, {x: 300, y: 13}, {x: 500, y: 11}]");
+  chart.setData('[{"x": 200, "y": 10}, {"x": 200, "y": 10}, {"x": 240, "y": 15}, {"x": 300, "y": 13}, {"x": 500, "y": 11}]');
   chart.calcExtrema();
 });
 
@@ -12,20 +12,36 @@ function Chart(container_id)
   var data;
 
   this.redraw = function(){
-
+    
   };
 
-  var biggestX;
-  var lowestX;
-  var biggestY;
-  var lowestY;
+  var maxX = -1000000;
+  var minX = 1000000;
+  var maxY = -1000000;
+  var minY = 1000000;
   this.calcExtrema = function(){
     for(i = 0; i < data.length; i++){
-      alert(data[i]);
+      var x = data[i].x;
+      if(x > maxX)
+        maxX = x;
+
+      if(x < minX)
+        minX = x;
+
+      var y = data[i].y;
+      if(y > maxY)
+        maxY = y;
+
+      if(y < minY)
+        minY = y;
     }
+
+    console.log("X: " + maxX + " <-> " + minX);
+    console.log("Y: " + maxY + " <-> " + minY);
   };
 
   this.setData = function(json){
     data = jQuery.parseJSON(json);
+    console.log(data);
   };
 }
